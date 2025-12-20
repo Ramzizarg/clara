1c1e0e843a060e244465a3d83d7a08de925300bf
Ramzizarg <Ramzi.zargelayoun@esprit.tn>
2025-12-20 18:11:08 +0100

    second commit

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -986,7 +986,7 @@ export default function ProductByIdPage() {
           />
 
           {/* Drawer */}
-          <div className="relative z-50 flex h-full w-64 flex-col bg-white shadow-2xl border-r border-zinc-200 transform -translate-x-full animate-[menuSlideIn_0.25s_ease-out_forwards]">
+          <div className="relative z-50 flex h-full w-64 flex-col bg-white shadow-2xl border-r border-zinc-200 translate-x-0 transition-transform duration-300 ease-out">
             <div className="flex items-center justify-between px-4 py-4 border-b border-zinc-200">
               <span
                 className="text-lg font-semibold tracking-tight text-zinc-900"
